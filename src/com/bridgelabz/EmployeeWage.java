789fbb16acce0c39444aee1180f8d4e8122c6bd0
DineshZingade <zingadedinesh1991@gmail.com>
2022-11-21 10:29:16 +0530

    Added: Employee Daily Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,14 +2,22 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 
-	public static void main(String[] arr) {
+//	UC2:Added Employee Daily Wages
+
+	static final int WAGE_PER_HOUR = 20;
+	static final int FULL_DAY_HOUR = 8;
+
+	public static void main(String[] args) {
+
 		int empIsPresent = (int) (Math.floor(Math.random() * 10) % 2);
+		int empWage = 0;
 		if (empIsPresent == 1) {
 			System.out.println("Employee is Present");
+			empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
 		} else {
 			System.out.println("Employee is Absent");
 		}
-
+		System.out.println("Employee Wage:" + empWage);
 	}
 
 }
